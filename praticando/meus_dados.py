572e27df947977f8b67fca582555f1987a733ae3
Nicolas <nicolascarreira2005@Gmail.com>
2024-11-03 15:29:13 -0300

    "Adicionando conteúdo ao arquivo de exercicio"

---
+++ praticando/meus_dados.py
@@ -0,0 +1,18 @@
+'''
+O exercicio a seguir consiste na prática dos conteúdos de: Variaveis, função print e tipos de dados
+
+A instrução é fazer uma espécie de "relatório" com os meus dados
+'''
+nome = 'Nicolas'
+sobrenome = 'Carreira'
+idade = 19
+ano_de_nascimento = 2005
+maior_de_idade = idade >= 18
+altura_metros = 1.72
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_de_nascimento)
+print('Maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
