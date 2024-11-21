6c69223fae7e4985a1ebe63c637eef2c025ab2fe
Nicolas <nicolascarreira2005@Gmail.com>
2024-11-21 17:10:46 -0300

    'Adicionando mudanças e arquivos 21/11/24'

--- parte_teorica/python_basico/5operadores_aritmeticos_e_relacionais.py
+++ parte_teorica/python_basico/5operadores_aritmeticos_e_relacionais.py
@@ -48,9 +48,38 @@ print('o' + 'i') # Irá nos retornar 'oi'
 print ('o' * 5) # Irá nos retornar 'ooooo'
 
 '''
-OPERADORES LÓGICOS
+OPERADORES RELACIONAIS
 
+Em python e na programação, na verdade, algo essencial são os operadores relacionais, também conhecidos como operadores de comparação.
+Os operadores de comparação são usados para checar coisas (se é menor, maior e etc..), sendo que na maioria esmagadora dos casos, nós utilizamos eles
+para fazer comparação entre varíaveis em condicionais ou loops. Além disso, apesar já termos visto isso em 'tipo de dados', é importante relembrar que comparadores 
+nos retornam valores booleanos (True ou Falso)
 
+Os operadores relacionais que temos são:
 
+a > b → operador que significa maior, “a” maior que “b”
+
+a >= b → operador que significa maior ou igual, “a” maior ou igual a “b”
+
+a < b → operador que significa menor, “a” menor que “b”
+
+a <= b → operador que significa menor ou igual, “a” menor ou igual a “b”
+
+“a” == “b” → operador que significa igual, “a” igual a “b”
+
+a != b → operador que significa diferente, “a” diferente de “b”
+
+Uma observação rápida é que usamos dois iguais (==) no operador relacional de  “igual” porque em python, um igual (=) significa atribuição, enquanto
+dois iguais significa CHECAR/COMPARAR se dois valores envolvidos são iguais
 
 '''
+
+print(1>2) # Irá nos retornar False
+
+print(1>=2) # Irá nos retornar False
+
+print(1<2) # Irá nos retornar True
+
+print(1<=2) # Irá nos retornar True
+
+print(1==2) # Irá nos retornar False
--- parte_teorica/python_basico/8loops.py
+++ parte_teorica/python_basico/8loops.py
@@ -9,13 +9,35 @@ Nós temos, no Python:
 
 Loop while (condição):
 
-O loop while irá repetir o trecho de código enquanto a condição dor verdadeira (True), o que é um pouco perigoso, pois muitas pessoas acabam não lembrando disso
-e se esquecendo de fazer uma forma para tornar a condição ao lado do while falsa (Falsa), criando um loop infinito. Sua estrutura é:
+O loop while irá repetir o trecho de código enquanto a condição for verdadeira (True), o que é um pouco perigoso, pois muitas pessoas acabam não lembrando disso
+e se esquecendo de fazer uma forma para tornar a condição ao lado do while falsa (False), criando um loop infinito. Sua estrutura é:
 
 while (condição):
     TRECHO DE CÓDIGO A SER REPETIDO
 
-Loop For:
+'''
+
+while True:
+    print('oi')
+
+    # Se pararmos por aqui e deixarmos nosso código assim, estaremos criando um loop infinito, pois a condição ao lado do while será SEMPRE True
+    # então, para evitar de criar um loop infinito nesse caso, podemos colocar um 'break', como fizemos abaixo
+    
+    break # O break busca o loop mais mais próximo dele e quebra. Dessa forma, nosso loop deixará de ser infinito e será controlado.
+
+# outra coisa que podemos fazer para que nosso loop não seja infinito é utilizar a atribuição, onde utilizaríamos uma váriavel e os operadores
+# lógicos para criar uma condição que possa se tornar falsa em algum momento, quebrando o loop.
+
+contador = 0 # Não colocamos a varíavel dentro do loop, pois seu valor iria reiniciar sempre que o loop rodasse e neste caso queremos ACUMULAR valores para parar o loop
+while contador < 10:
+    print('oi')
+    
+    contador += 1 # Dessa forma, cada vez que o loop rodar iremos adicionar +1 à variavel 'contador' de forma a acumular valores, até o momento que nossa váriavel 
+                  # 'contador' estiver com um valor maior ou igual a 10, encerrando assim o loop.
+
+'''
+Além do loop while, outro loop que temos no pyhton é o Loop 'for':
+
 
 
 
