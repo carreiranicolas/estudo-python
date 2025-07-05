c3c136847b4958e2e6872025ca961442105c8a1e
Nicolas <nicolascarreira2005@gmail.com>
2025-07-05 11:11:52 -0300

    Estou fazendo a parte de funcoes. Parei em args

--- parte_teorica/python_intermediario/funcoes/args_para_argumentos_n_nomeados.py
+++ parte_teorica/python_intermediario/funcoes/args_para_argumentos_n_nomeados.py
@@ -2,7 +2,7 @@
 *ARGS PARA A QUANTIDADE DE ARGUMENTOS NÃO NOMEADOS VARIÁVEIS
 
 Bom, já vimos sobre parâmetros e argumentos que é algo de muita importante em funções. Vimos isso no arquivo
-de conceitos. Inclusive, uma coisa que vimos no arquivo de conceito é que o print é uma função.
+de conceitos.py. Inclusive, uma coisa que vimos no arquivo de conceito é que o print é uma função.
 
 Algo muito importante é que se pararmos para observar, a função print pode receber inúmeros argumentos sem dar um
 bug dizendo que passamos um número de argumentos não correspondente ao número de parâmetros da função. 
@@ -29,12 +29,23 @@ e isso traz segurança, uma vez que argumentos não nomeados são apenas valores
 
 No entanto, se quiséssemos transformar em uma lista, que é um tipo mutável, poderíamos fazer isso da seguinte forma:
 
+def soma(*args):
+    args = list(args)
+    print(args)
+
+soma(1,2,3,4,5) # Irá retornar [1, 2, 3, 4, 5]
+
 
 Agora voltando, para nós fazermos a soma de todos os valores que passamos no argumento, nós precisaríamos iterar a 
 tupla a partir de um loop for e ir acumulando esses valores em uma variável, assim como fizemos abaixo:
 
 
 Assim, ele iteraria, faria o acumulo dos números no total e depois nos retornaria o total
+
+
+Uma observação é se criássemos uma variável e passassemos os valores que queremos fazer a soma para ela e depois 
+passassemos essa variavel para a nossa função soma, isso não daria certo, porque quando passamos varios valores em 
+uma varíavel  isso irá virar uma tupla. Dessa forma, como passamos uma tupla como argumento, lembre que o que o *args vai fazer será empacotar os argumentos que passamos para a função em uma tupla, portanto teríamos uma tupla dentro de uma tupla e aí ficaria tudo bugado e nos retornaria o erro que vimos. 
 
 '''
 
